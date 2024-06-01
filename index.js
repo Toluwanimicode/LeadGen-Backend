c97fd64679f6007e63b15c388948b89153eb6db7
Toluwanimicode <da.toluwanimi@gmail.com>
2024-06-01 14:17:35 +0100

    added index .js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const routes = require('./routes');
@@ -12,21 +11,22 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Connect to MongoDB using the connection string from environment variables
-mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => {
-        console.log("Connected to the database");
+mongoose.connect(process.env.MONGODB_URL)
+  .then(() => {
+    console.log("Connected to the database");
 
-        // Apply CORS middleware
-        app.use(cors());
-        app.use(bodyParser.json());
+    // Apply CORS middleware
+    app.use(cors());
+    app.use(express.json());
 
-        // Define routes
-        app.use('/', routes);
+    // Define routes
+    app.use('/', routes);
 
-        app.listen(PORT, () => {
-            console.log(`Server is running on port ${PORT}`);
-        });
-    })
-    .catch(err => {
-        console.error("Failed to connect to the database", err);
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
     });
+  })
+  .catch((error) => {
+    console.error("Error connecting to the database:", error);
+  });
+
