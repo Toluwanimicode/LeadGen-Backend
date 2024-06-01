61347acac1580217c5966076a1c1cb49804105bd
Toluwanimicode <da.toluwanimi@gmail.com>
2024-06-01 14:04:24 +0100

    Secure MongoDB connection using environment variables

--- index.js
+++ index.js
@@ -1,26 +1,32 @@
-// backend/index.js
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const routes = require('./routes');
+const dotenv = require('dotenv');
+
+// Load environment variables from .env file
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect("mongodb+srv://DAT:Tolu1329@cluster0.oetks32.mongodb.net/Subscribecollection?retryWrites=true&w=majority")
-.then(() => {
-    console.log("Connected to the database");
+// Connect to MongoDB using the connection string from environment variables
+mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log("Connected to the database");
 
-     // Apply CORS middleware
+        // Apply CORS middleware
         app.use(cors());
         app.use(bodyParser.json());
 
-    // Define routes
-    app.use('/', routes);
+        // Define routes
+        app.use('/', routes);
 
-    app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
     })
-
-})
+    .catch(err => {
+        console.error("Failed to connect to the database", err);
+    });
