c729541734b703c3b3fd07c458447378273783d5
Toluwanimicode <da.toluwanimi@gmail.com>
2024-06-03 14:01:08 +0100

    added mongodb configurations

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Connect to MongoDB using the connection string from environment variables
-mongoose.connect(process.env.MONGODB_URL)
+mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log("Connected to the database");
 
@@ -29,4 +29,5 @@ mongoose.connect(process.env.MONGODB_URL)
   .catch((error) => {
     console.error("Error connecting to the database:", error);
   });
+
 
